45644f99c9ae503392bdec3baf56c1be5ca14d98
yskoverride <yskoverride@gmail.com>
2017-01-28 22:28:29 +0530

    Added ResetPassword Controller

---
+++ app/Http/Controllers/SellerAuth/ResetPasswordController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\SellerAuth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ResetPasswordController extends Controller
+{
+    //
+}
