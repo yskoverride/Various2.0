3d93e38f30318059f34cfd35afa40ead8495dbc6
yskoverride <yskoverride@gmail.com>
2017-01-28 21:58:16 +0530

    Added ForgotPassword Controller

---
+++ app/Http/Controllers/SellerAuth/ForgotPasswordController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\SellerAuth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ForgotPasswordController extends Controller
+{
+    //
+}
